eb8e770d28bf0d6d8ab64953d4a8127aaa0efe46
dewaary <dewa.g.ari28@gmail.com>
2021-08-14 01:16:45 +0700

    [FE] Pages Home Design Featured

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import FeatureInfo from '../../components/featuredInfo/FeatureInfo'
+import "./home.css"
+
+export default function Home() {
+    return (
+        <div className="home">
+            <FeatureInfo />
+        </div>
+    )
+}
